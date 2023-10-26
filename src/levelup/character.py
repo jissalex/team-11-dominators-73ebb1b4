04473d918b439ee78b446587dcfd4f2a2ec505ad
Prem Murugesan <premmsn@yahoo.com>
2023-10-26 15:14:12 -0400

    fakemmap

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,6 +1,8 @@
 class Character:
     name = ""
-    length=""
 
     def __init__(self, character_name):
       self.name = character_name
+
+    def enter_map(self, map):
+        pass
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,16 +1,26 @@
 from unittest import TestCase
 from levelup.character import Character
 
+class Fakemap:
+    pass
+
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
 
-class TestCharacterLessThanTwoLetters(TestCase):
-    def test_init(self):
-        ARBITARY_LENGTH = "4"
-        testobj = Character(ARBITARY_LENGTH)
-        self.assertEqual(ARBITARY_LENGTH, testobj.length)
-
+#class TestCharacterLessThanTwoLetters(TestCase):
+#   def test_init(self):
+#        ARBITARY_LENGTH = "4"
+#        testobj = Character(ARBITARY_LENGTH)
+#        self.assertEqual(ARBITARY_LENGTH, testobj.length)
 #create fake map underneath this
+
+    def test_enter_map(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        fakemap = Fakemap()
+        testobj.enter_map(fakemap)
+
+        
