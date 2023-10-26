d586664bc8e1aa21c9ccae524d51f3ddd2505b40
Prem Murugesan <premmsn@yahoo.com>
2023-10-26 14:49:23 -0400

    modified test_character

--- tests/test_character.py
+++ tests/test_character.py
@@ -6,3 +6,11 @@ class TestCharacterInitWithName(TestCase):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+class TestCharacterLessThanTwoLetters(TestCase):
+    def test_init(self):
+        ARBITARYY_LENGTH = "4"
+        testobj = Character(ARBITARY_LENGTH)
+        self.assertEqual(ARBITARYY_LENGTH, testobj.length)
+
+#create fake map underneath this
