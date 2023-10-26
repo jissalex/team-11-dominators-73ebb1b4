5bedbc1b83f5d6e4160641ac6dc336335654f948
Prem Murugesan <premmsn@yahoo.com>
2023-10-26 17:37:35 -0400

    updated char files

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,22 +1,3 @@
-
-class Character:
-    name = "Fire"
-    current_position = 100 
-
-    def __init__(self, character_name):
-      self.name = character_name
-
-    def enter_map(self, map):
-        pass
-    
-    def get_Name(self):
-        return self.name
-    
-    def get_Position(self):
-        return self.current_position
-
-
-##copied from Level-Up-Sync
 from levelup.position import Position
 from levelup.direction import Direction
 from levelup.map import Map
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,34 +1,28 @@
 from unittest import TestCase
 from levelup.character import Character
+from fake_map import FakeMap
+from levelup.controller import Direction
 
-class Fakemap:
-    pass
+class TestCharacter(TestCase):
+    ARBITRARY_NAME = "MyName"
 
-class FakePos:
-    pass
-
-class TestCharacterInitWithName(TestCase):
     def test_init(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        self.assertEqual(ARBITRARY_NAME, testobj.name)
+        testobj = Character(self.ARBITRARY_NAME)
+        self.assertEqual(self.ARBITRARY_NAME, testobj.name)
 
-    def test_enter_map(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        fakemap = Fakemap()
-        testobj.enter_map(fakemap)
-        expected_position = 100
-        self.assertEqual(testobj.current_position, expected_position)
-    
-    def test_get_name(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        self.assertEqual(testobj.get_Name(), 'MyName')
+    def test_enter_map_sets_map_and_updates_position(self):
+        testobj = Character(self.ARBITRARY_NAME)
+        stubbed_map = FakeMap()
+        testobj.enter_map(stubbed_map)
+        self.assertEqual(stubbed_map, testobj.map)
+        self.assertEqual(testobj.current_position, stubbed_map.starting_position)
 
-    def test_get_position(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        self.assertEqual(testobj.get_Position(), 100)
-    
+    def test_move_updates_position(self):
+        testobj = Character(self.ARBITRARY_NAME)
+        stubbed_map = FakeMap()
+        testobj.map = stubbed_map
+        
+        testobj.move(Direction.EAST)
 
+        self.assertEqual(stubbed_map.STUBBED_X, testobj.current_position.x)
+        self.assertEqual(stubbed_map.STUBBED_Y, testobj.current_position.y)
